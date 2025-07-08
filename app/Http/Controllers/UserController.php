e57a279ffd751649694afa96f5596b326504a11a
Ruslanio <ruslanep2@gmail.com>
2025-07-08 15:34:51 +0300

    added prepareLikedPosts and kicking posts from feed if post like

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\Post\StoreRequest;
 use App\Http\Resources\Post\PostResource;
 use App\Http\Resources\User\UserResource;
+use App\Models\LikedPost;
 use App\Models\Post;
 use App\Models\PostImage;
 use App\Models\SubscriberFollowing;
@@ -18,47 +19,74 @@ class UserController extends Controller
 
     public function index()
     {
-        $users = User::whereNot('id',  auth()->id())
+        $users = User::whereNot('id', auth()->id())
             ->withCount('posts')
             ->get();
 
         $followingIds = SubscriberFollowing::where('subscriber_id', auth()->id())
-
             ->pluck('following_id')
             ->toArray();
 
         foreach ($users as $user) {
-            if(in_array($user->id, $followingIds)) {
+            if (in_array($user->id, $followingIds)) {
                 $user->is_followed = true;
             }
         }
         return UserResource::collection($users);
     }
 
-    public function post(User $user) {
-       return PostResource::collection($user->posts);
+    public function post(User $user)
+    {
+        $posts = $user->posts()->latest()->get();
+        $posts = $this->prepareLikedPosts($posts);
+
+        return PostResource::collection($posts);
     }
 
 
     public function toggleSubscribe(User $user)
     {
-       $res = auth()->user()->followings()->toggle($user->id);
+        $res = auth()->user()->followings()->toggle($user->id);
 
-       $data['is_followed'] = count($res['attached']) > 0;
+        $data['is_followed'] = count($res['attached']) > 0;
 
-       return $data;
+        return $data;
     }
-
-
 
 
     public function followingPost()
     {
-        $followedIds = auth()->user()->followings()->get()->pluck('id')->toArray();
-        $posts = Post::whereIn('user_id', $followedIds)->get();
+        $followedIds = auth()->user()->followings()->latest()->get()->pluck('id')->toArray();
+
+
+        $likedPostIds = LikedPost::where('user_id', auth()->id())
+            ->pluck('post_id')
+            ->toArray();
+
+        $posts = Post::whereIn('user_id', $followedIds)
+            ->whereNotIn('id', $likedPostIds)
+            ->get();
+
+
 
         return PostResource::collection($posts);
 
     }
+
+    private function prepareLikedPosts($posts)
+    {
+        $likedPostIds = LikedPost::where('user_id', auth()->id())
+            ->pluck('post_id')
+            ->toArray();
+
+        foreach ($posts as $post) {
+            if (in_array($post->id, $likedPostIds)) {
+                $post->is_liked = true;
+            }
+        }
+
+        return $posts;
+    }
+
 
 }
